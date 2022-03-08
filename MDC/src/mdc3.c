5da63f6b52cddf3a710ff8edc5a1b521c0bdf90b
Yuri Feitosa <yuri.rgf@gmail.com>
2022-03-08 00:02:35 -0300

    Comentários.

--- MDC/src/mdc3.c
+++ MDC/src/mdc3.c
@@ -3,6 +3,12 @@
 #include <math.h>
 #include "mdc.h"
 
+/*
+Terceira versão do código: O máximo divisor comum (MDC)
+Além do tratamento dos dados de entrada sejam iguais a zero, foi incluido a função ABS, que retorna o valor absoluto no final da função.
+Esse recurso foi implementado pára o algoritmo trazer o resultado correto no caso do usuário entrar com valores negativos.
+*/
+
 int mdc (int a, int b){
 	int resto;
 	if ((a==0)||(b==0)){
